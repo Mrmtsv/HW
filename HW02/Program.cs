c9922a482fca73ab44fcb9eb534aba6a1a1e74dc
MuromtsevVV <bajiau@mail.ru>
2023-08-15 16:10:12 +0300

    Added HW04

--- HW02/Program.cs
+++ HW02/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите первое число");
+﻿Console.WriteLine("Введите первое число");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int numberB = Convert.ToInt32(Console.ReadLine());
