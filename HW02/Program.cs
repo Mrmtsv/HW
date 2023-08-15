7a59c6911d539f8e1e1e72332aff5b1e408c57c6
MuromtsevVV <bajiau@mail.ru>
2023-08-15 15:53:18 +0300

    Correction HW02

--- HW02/Program.cs
+++ HW02/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA < numberB)
+{
+Console.WriteLine($"Число {numberB} большее, а {numberA} - меньшее");
+}
+if (numberB < numberA)
+{
+Console.WriteLine($"Число {numberA} большее, а {numberB} - меньшее");
+}
+if (numberB == numberA)
+{
+Console.WriteLine($"Числа {numberA} и {numberB} равны");
+}
