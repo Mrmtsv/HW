e7de19af76e6c26cd475fd215143ca46b3b90553
Vladimir <bajiau@mail.ru>
2023-08-18 19:43:09 +0300

    Added HW15

---
+++ HW15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+if ((digit > 0) & (digit < 6))
+{
+    Console.WriteLine("Нет");
+}
+else
+{
+    Console.WriteLine("Да");
+}
