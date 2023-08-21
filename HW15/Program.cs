e5b3703cb02abf81add596344b251c1485bb480b
MuromtsevVV <bajiau@mail.ru>
2023-08-21 10:00:54 +0300

    Corrected HW15

--- HW15/Program.cs
+++ HW15/Program.cs
@@ -1,11 +1,17 @@
 ﻿Console.WriteLine("Введите цифру, обозначающую день недели");
 int digit = Convert.ToInt32(Console.ReadLine());
-
-if ((digit > 0) & (digit < 6))
+if ((digit > 7) || (digit < 1))
 {
-    Console.WriteLine("Нет");
+    Console.WriteLine("Введенная цифра не может обозначать день недели");
 }
 else
 {
-    Console.WriteLine("Да");
+    if ((digit > 0) & (digit < 6))
+    {
+        Console.WriteLine("Не является выходным.");
+    }
+    else
+    {
+        Console.WriteLine("Выходной!");
+    }
 }
